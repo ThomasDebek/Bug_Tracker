e6e87602c353b8a0dbe6af556df0e705b422c82d
ThomasDebek <tom.debek@gmail.com>
2026-01-29 21:18:46 +0100

    routes: added path and used shallow :) fuck zajebioza

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :issues
-  resources :projects
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'projects#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :projects do
+    resources :issues, shallow: true do
+      resources :comments, only: [:create]
+    end
+  end
+
+
+
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
