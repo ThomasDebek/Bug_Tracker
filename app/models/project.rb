d4a2094ff17244f7b89860a2c1f25de59f0a29ec
ThomasDebek <tom.debek@gmail.com>
2026-01-29 21:59:30 +0100

    model: project adde new method

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,21 @@
 class Project < ApplicationRecord
   has_many :issues
   validates :name, presence: true
+
+  def open_issues_count
+    issues.open.count
+  end
+
+  def in_progress_issues_count
+    issues.in_progress.count
+  end
+
+  def closed_issues_count
+    issues.closed.count
+  end
+
+  def overdue_issues_count
+    issues.overdue.count
+  end
+
 end
