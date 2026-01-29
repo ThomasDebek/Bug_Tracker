d0cd857b0e60b62f458aa8b75559efceb9bdcd97
ThomasDebek <tom.debek@gmail.com>
2026-01-29 21:42:46 +0100

    controller: create new comments and method create

---
+++ app/controllers/coments_controller.rb
@@ -0,0 +1,20 @@
+class ComentsController < ApplicationController
+
+  def create
+    @issue = Issue.find(params[:issue_id])
+    @comment = @issue.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to @issue
+    else
+      redirect_to @issue, alert: "Comment cannot be empty"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+  
+end
