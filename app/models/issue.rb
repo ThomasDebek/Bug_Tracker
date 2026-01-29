4a7184e56d7fcd12644e24699e6f24c4fc4c400b
ThomasDebek <tom.debek@gmail.com>
2026-01-29 21:23:26 +0100

    enum: for status and priority

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,9 @@ class Issue < ApplicationRecord
   belongs_to :project
   has_many :comments, dependent: :destroy
 
+
+  enum status: { open: 0, in_progress: 1closed: 2 }
+  enum priority: { low: 0, medium: 1, high: 2 }
+
+
 end
